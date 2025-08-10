f7fce1350c937f8eafc877bf5ffd9127c29c6414
amctech106 <121874402+amctech106@users.noreply.github.com>
2025-08-10 11:54:17 +0500

    Functions
    Types of Functions and examples.

---
+++ Functions/script.js
@@ -0,0 +1,29 @@
+///There are three types of Functions.
+
+//1. Regular Function or Function declearation 
+//2. Funtion Expression-----------------------
+//3. Fat Aero Function------------------------
+
+///Function Declearation-----
+
+function abc (){
+    console.log("Happy New Year");
+}
+
+abc ();
+
+////Function Expression----------------------
+
+let xyz =function(){
+    console.log("Welcome to JavaScript");
+}
+
+xyz();
+
+//Fat Aero Function
+
+let abcd = ()=>{
+console.log("Hello!");
+}
+
+abcd();
